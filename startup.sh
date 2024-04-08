62b78278b7e6caf1188427eb56dbac73f2352bf6
Kyle Hughes <Hugh472@users.noreply.github.com>
2024-04-08 14:47:50 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,24 +2,32 @@
 
 # TODO: Turn this into Ansible
 
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
-yay -Syu
+LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" -Syu
 
 # If macbook pro 2013, if not comment out the following file
-yay -S nvidia-470xx-dkms nvidia-470xx-utils lib32-nvidia-470xx-utils opencl-nvidia-470x
+LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" -S nvidia-470xx-dkms nvidia-470xx-utils lib32-nvidia-470xx-utils opencl-nvidia-470x
 
 #If not
 # sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils
 
+# TODO: Firefox add-ons automation if possible
+
+# Thunderbird, calibre, ardour, inkscape, blender
+sudo pacman -S --noconfirm thunderbird calibre ardour inkscape blender
+
+# OnlyOffice
+LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" -S onlyoffice-bin
+
 # Debugging tools and Steam
-sudo pamcan -S vulkaninfo nvidia-settings steam
+sudo pamcan -S --noconfirm vulkaninfo nvidia-settings steam
 
 read -p "Can reboot here to test if Steam games will work.  If you want to continue instead, Press enter"
 
 # install dev tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-sudo pacman -S nerd-fonts starship
-yay -S wezterm vscodium zed-preview-editor
+sudo pacman -S --noconfirm nerd-fonts starship
+LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" -S wezterm vscodium zed-preview-editor
 
